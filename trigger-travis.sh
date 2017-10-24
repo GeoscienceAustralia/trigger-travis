c1e157f8a648cbf90a18a75e29408af3bf9a3820
lbodor <lazar.bodor@ga.gov.au>
2017-10-24 14:20:58 +1100

    Add switch to specify branch to build

--- trigger-travis.sh
+++ trigger-travis.sh
@@ -2,16 +2,36 @@
 
 # Trigger builds of downstream dependencies.
 #
-# Usage: declare -a downstream=("project-a" "project-b"); trigger-travis "${downstream[@]}"
+# Usage: declare -a downstream=("project-a" "project-b"); trigger-travis [-b <branch>] "${downstream[@]}"
 # Notes:
 #   1) In a Travis build job, TRAVIS_PULL_REQUEST is either set to "false" or to the pull request number.
 #   2) Use travis CLI to generate and ecrypt the API token TRAVIS_TOKEN.
+#   3) Default branch to trigger is master
+
 function trigger-travis {
+    branch="master"
+    positional=()
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -b|--branch)
+            branch="$2"
+            shift
+            shift
+            ;;
+            *)
+            positional+=("$1")
+            shift
+            ;;
+        esac
+    done
+
+    set -- "${positional[@]}"
+
     if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
         for repo in "$@"; do
             body='{
             "request": {
-              "branch":"master"
+              "branch":"'"${branch}"'"
             }}'
 
             curl -s -X POST \
